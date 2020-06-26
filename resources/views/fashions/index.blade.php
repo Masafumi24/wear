19f428d0e3936ac43d3e048e267638bd26cfcb96
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-06-26 14:08:05 +0900

    DB Data -> view OK

--- resources/views/fashions/index.blade.php
+++ resources/views/fashions/index.blade.php
@@ -1,1 +1,14 @@
-@extends('template')
+@extends('header')
+
+@section('content')
+  <div class="fashionIndex">
+    @foreach($fashions as $fashion)
+      <tr>
+        <td>{{ $fashion->title }}</td>
+        <td>
+          <span class="label">{{ $fashion->introduction }}</span>
+        </td>
+      </tr>
+    @endforeach
+  </div>
+@endsection
