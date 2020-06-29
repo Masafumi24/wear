23a815e31c40ccc9fcf37809531e69d57cca8464
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-06-29 13:07:29 +0900

    sidebar OK

--- resources/views/fashions/index.blade.php
+++ resources/views/fashions/index.blade.php
@@ -5,4 +5,58 @@
     <div class="likeBtn">おすすめ</div>
     <div class="timelineBtn">タイムライン</div>
   </div>
+  <div class="fashionIndexMain">
+    <div class="side">
+      <div class="sideOneContent">
+        <p class="sideOneContentTitle">探す</p>
+        <a href="#" class="sideBtn">コーディネートを探す</a>
+        <a href="#" class="sideBtn">ユーザーを探す</a>
+        <a href="#" class="sideBtn">アイテムを探す</a>
+        <a href="#" class="sideBtn">ショップを探す</a>
+        <a href="#" class="sideBtn">ブランドを探す</a>
+      </div>
+      <div class="sideOneContent">
+        <p class="sideOneContentTitle">ユーザー</p>
+        <a href="#" class="sideBtn">テスト</a>
+        <a href="#" class="sideBtn">テスト</a>
+        <a href="#" class="sideBtn">テスト</a>
+        <a href="#" class="sideBtn">テスト</a>
+        <a href="#" class="sideBtn">テスト</a>
+      </div>
+      <div class="sideOneContent">
+        <p class="sideOneContentTitle">ブランド</p>
+        <a href="#" class="sideBtn">テストブランド</a>
+        <a href="#" class="sideBtn">テストブランド</a>
+        <a href="#" class="sideBtn">テストブランド</a>
+        <a href="#" class="sideBtn">テストブランド</a>
+        <a href="#" class="sideBtn">テストブランド</a>
+      </div>
+      <div class="sideOneContent">
+        <p class="sideOneContentTitle">急上昇ワード</p>
+        <a href="#" class="sideBtn">テストワード</a>
+        <a href="#" class="sideBtn">テストワード</a>
+        <a href="#" class="sideBtn">テストワード</a>
+        <a href="#" class="sideBtn">テストワード</a>
+        <a href="#" class="sideBtn">テストワード</a>
+      </div>
+      <div class="sideOneContent">
+        <p class="sideOneContentTitle">人気のカテゴリー</p>
+        <a href="#" class="sideBtn">テストカテゴリー</a>
+        <a href="#" class="sideBtn">テストカテゴリー</a>
+        <a href="#" class="sideBtn">テストカテゴリー</a>
+        <a href="#" class="sideBtn">テストカテゴリー</a>
+        <a href="#" class="sideBtn">テストカテゴリー</a>
+      </div>
+    </dav>
+    <div class="main">
+      <div class="mainContent">
+        <h1 class="mainContentTitle">テスト</h1>
+        <div class="mainContentBox">
+          <div class="mainContentBoxList"></div>
+          <div class="mainContentBoxList"></div>
+          <div class="mainContentBoxList"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
