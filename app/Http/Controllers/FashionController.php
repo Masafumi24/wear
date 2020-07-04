4920b183ab4a88e9263ebc6b03b7fb6c00ccdef1
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-07-04 15:02:22 +0900

    Update FashionController.php

--- app/Http/Controllers/FashionController.php
+++ app/Http/Controllers/FashionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Fashion;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateFashion;
 
 class FashionController extends Controller
 {
@@ -23,6 +24,15 @@ class FashionController extends Controller
 
     public function create(CreateFahion $request)
     {
+        $fashion = new Fashion();
 
+        $fashion->introduction = $request->introduction;
+        $fashion->path = $request->path;
+        $fashion->sex = $request->sex;
+        $fashion->height = $request->height;
+        $fashion->age = $request->age;
+        $fashion->hairstyle = $request->hairstyle;
+
+        return redirect()->route('/');
     }
 }
